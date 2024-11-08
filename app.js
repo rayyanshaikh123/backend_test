ce8cbdd89c3f4a715632a87fa61647410defc5d3
rayyanshaikh123 <143872829+rayyanshaikh123@users.noreply.github.com>
2024-11-08 12:22:18 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+app.use(cors({ origin: 'https://bitmasters52.online' }));
+require('dotenv').config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(() => console.log("Connected to MongoDB")).catch((error) => console.error("MongoDB connection error:", error));
+
+// Routes
+app.use('/tasks', require('./routes/tasks'));
+
+// Start server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
